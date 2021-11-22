ee51e17a5a2c507240251828947acc2d4e16fab3
Rodrigo Castro <rodrcastro@gmail.com>
2021-11-22 21:38:42 +0000

    add slideshow done in class with teacher

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,89 +1,129 @@
-// TODO - Adicionar as setas
-// TODO - Criar lógica para a seta para esquerda
-// TODO - Criar lógica para click dos dots
+// TODO: Definir intervalo para slideshow voltar a rodar automaticamente depois da interação do usuário
 
-document.querySelector('.slideshow__dots').addEventListener('click', onDotClick);
+/* 
+Perguntas:
+- Quando percebi que ia utilizar novamente muito da lógica anterior, tentei tirar as consts de dentro da function nextSlide pra poder usá-las globalmente, mas quebrou a funcionalidade de rodar os slides automaticamente. Por quê?
+  - Não serve de muito - sempre vão ser distintas chamadas e vários valores diferentes sempre, então tanto faz se isto está dentro da function, uma vez que ela vai ser chamada
+  - Funcionaria, mas pelo que está acima, não valeria a pena
+- Não consegui colocar setas de verdade nos botões esquerda e direita
+*/
 
-function onDotClick(event){
-    const dotClicked = event.target.closest('.slideshow__dot');
-    
-    if(dotClicked){
-        const index = Array.from(dotClicked.parentElement.children).indexOf(dotClicked);
+// let currentSlide = 0;
 
-        document.querySelector('.slideshow__dot--active').classList.remove('slideshow__dot--active');
-        dotClicked.classList.add('slideshow__dot--active');
-        document.querySelector('.slideshow__item--active').classList.remove('slideshow__item--active');
-        document.querySelector(`.slideshow__item:nth-child(${index+1})`).classList.add('slideshow__item--active');
-    }
-}
-
-
-function nextSlide() {
-  // Buscar primeiro qual slide é que está ativo
-  //  const currentDot = document.querySelector('.slideshow__dot--active'); - Necessário apenas no método comentado
-  const currentSlide = document.querySelector('.slideshow__item--active');
-
-
- // Outra forma de fazer o mesmo processo abaixo (retira os ifs)
+function changeSlide(currentSlide, element) {
+  // Removemos a classe do Slideshow ativo
   currentSlide.classList.remove('slideshow__item--active');
 
-
-  const next = currentSlide.nextElementSibling || currentSlide.parentElement.firstElementChild;
-
-  next.classList.add('slideshow__item--active');
-
-  const index = Array.from(next.parentElement.children).indexOf(next);
-
+  // Removemos a classe do Dot Ativo
   document.querySelector('.slideshow__dot--active').classList.remove('slideshow__dot--active');
 
+  // Adicionamos a classe ativa para o slide atual
+  element.classList.add('slideshow__item--active');
+  const index = Array.from(element.parentElement.children).indexOf(element);
+
+  // Adicionamos a classe ativa para o Dot do index correto
   document.querySelector(`.slideshow__dot:nth-child(${index + 1})`).classList.add('slideshow__dot--active');
 
 }
 
-document.querySelector('.slideshow__arrows').addEventListener('click', onArrowClick);
+function prevSlide() {
+  const currentSlide = document.querySelector('.slideshow__item--active');
+  const prev = currentSlide.previousElementSibling || currentSlide.parentElement.lastElementChild;
+  changeSlide(currentSlide, prev);
+}
 
-function onArrowClick(event) {
-  const arrowClicked = event.target;
+function nextSlide() {
   const currentSlide = document.querySelector('.slideshow__item--active');
   const next = currentSlide.nextElementSibling || currentSlide.parentElement.firstElementChild;
-  const previous = currentSlide.previousElementSibling || currentSlide.parentElement.lastElementChild;
-  const index = Array.from(next.parentElement.children).indexOf(next);
-  const previousIndex = Array.from(previous.parentElement.children).indexOf(previous);
+  changeSlide(currentSlide, next);
+}
+
+
+function onDotClick(event){
+  /* Feito em Aula:
+   const { target } = event; - destructuring
+   if(target.classList.contains('slideshow__dot')) {
+      const index = Array.from(target.parentElement.children).indexOf(target);
+      const currentSlide = document.querySelector('.slideshow__item--active');
+      const next = document.querySelector(`.slideshow__item:nth-child(${index + 1})`)
+
+      changeSlide(currentSlide, next);
+  }
+
+*/
+
+  const dotClicked = event.target.closest('.slideshow__dot');
+   if(dotClicked){
+      const index = Array.from(dotClicked.parentElement.children).indexOf(dotClicked);
+      document.querySelector('.slideshow__dot--active').classList.remove('slideshow__dot--active');
+      dotClicked.classList.add('slideshow__dot--active');
+      document.querySelector('.slideshow__item--active').classList.remove('slideshow__item--active');
+      document.querySelector(`.slideshow__item:nth-child(${index+1})`).classList.add('slideshow__item--active');
+      clearInterval(play);
+    }
+}
+
+document.querySelector('.slideshow__dots').addEventListener('click', onDotClick);
+
+function onKeypress(event) {
+  if(event.code === 'ArrowLeft') {
+    clearInterval(play);
+    prevSlide();
+  } else if(event.code === 'ArrowRight') {
+    clearInterval(play);
+    nextSlide();
+  }
+}
+
+window.addEventListener('keyup', onKeypress);
+
+function onArrowClick(event) {
+  clearInterval(play);
+  const arrowClicked = event.target;
+  // const currentSlide = document.querySelector('.slideshow__item--active');
+  // const next = currentSlide.nextElementSibling || currentSlide.parentElement.firstElementChild;
+  // const index = Array.from(next.parentElement.children).indexOf(next);
+  // const previous = currentSlide.previousElementSibling || currentSlide.parentElement.lastElementChild;
+  // const previousIndex = Array.from(previous.parentElement.children).indexOf(previous);
 
   if (arrowClicked.className === 'arrow__right') {
-    currentSlide.classList.remove('slideshow__item--active');
-    next.classList.add('slideshow__item--active');
+    nextSlide();
+    // Retirado código abaixo por conta da função nextSlide()
+    // currentSlide.classList.remove('slideshow__item--active');
+    // next.classList.add('slideshow__item--active');
+    // document.querySelector('.slideshow__dot--active').classList.remove('slideshow__dot--active');
+    // document.querySelector(`.slideshow__dot:nth-child(${index + 1})`).classList.add('slideshow__dot--active');
 
-    document.querySelector('.slideshow__dot--active').classList.remove('slideshow__dot--active');
-    document.querySelector(`.slideshow__dot:nth-child(${index + 1})`).classList.add('slideshow__dot--active');
   } else {
-    currentSlide.classList.remove('slideshow__item--active');
-    previous.classList.add('slideshow__item--active');
-    document.querySelector('.slideshow__dot--active').classList.remove('slideshow__dot--active');
-    document.querySelector(`.slideshow__dot:nth-child(${previousIndex + 1})`).classList.add('slideshow__dot--active');
+    prevSlide();
+    // Retirado código abaixo por conta da função prevSlide()
+    // currentSlide.classList.remove('slideshow__item--active');
+    // previous.classList.add('slideshow__item--active');
+    // document.querySelector('.slideshow__dot--active').classList.remove('slideshow__dot--active');
+    // document.querySelector(`.slideshow__dot:nth-child(${previousIndex + 1})`).classList.add('slideshow__dot--active');
   }
-    // dotClicked.classList.add('.slideshow__dot')
-    // document.querySelector('.slideshow__item--active').classList.remove('slideshow__item--active');
-    // document.querySelector(`.slideshow__item:nth-child(${index+1})`).classList.add('slideshow__item--active');
-  }
+}
+
+document.querySelector('.slideshow__arrows').addEventListener('click', onArrowClick);
 
 
-
-  // // Retira o class desse slide
+  // const currentDot = document.querySelector('.slideshow__dot--active'); - Necessário apenas no método comentado
+ // Outra forma de fazer o mesmo processo abaixo (retira os ifs)
+  // Retira o class desse slide
   // currentSlide.classList.remove('slideshow__item--active');
   // currentDot.classList.remove('slideshow__dot--active');
 
-  // // Se o item que estiver ativo tiver um irmão, adiciona a classe de active
+  // Se o item que estiver ativo tiver um irmão, adiciona a classe de active
   // if(currentSlide.nextElementSibling) {
   //   currentSlide.nextElementSibling.classList.add('slideshow__item--active');
   //   currentDot.nextElementSibling.classList.add('slideshow__dot--active')
 
-  // // Se não tiver um irmão, busca o primeiro item para mostrar esse
+  // Se não tiver um irmão, busca o primeiro item para mostrar esse
   // } else {
   //   currentSlide.parentElement.firstElementChild.classList.add('slideshow__item--active');
   //   currentDot.parentElement.firstElementChild.classList.add('slideshow__dot--active');
   // }
 //setTimeout(nextSlide, 5000);
-// setInterval(nextSlide, 5000);
+let play = setInterval(nextSlide, 5000);
 
 
