75c80a16b8d7dbff0a63f410e46d0925e604c8e6
Rodrigo Castro <rodrcastro@gmail.com>
2021-12-02 12:50:21 +0000

    add client functionality

--- tasks-webapp/js/components/clients/clients.js
+++ tasks-webapp/js/components/clients/clients.js
@@ -1,2 +1,145 @@
 import './clients.scss';
 import '../client/client';
+
+/*
+Passos a seguir:
+- Perde o atributo de disabled quando algo estiver digitado no input
+- Adicionar a um array de clients vazio o client que foi digitado no input
+- Ao clicar em add, criar o HTML do cliente
+*/
+
+/*
+- Criar funcionalidade de delete e edit
+
+- Devo saber qual é o item que estou clicando (sempre lembrar de tirar um item do array que vou buscar pq tem o "p" do Name)
+
+**DELETE**
+- Deve apagar o element
+- Apagar o elemento do array
+- Guardar no storage do array
+
+**EDIT**
+- Remove o atributo do input
+- Ao sair do input (blur, quando perde o foco) atualizar o valor no array
+- Atualizar o storage
+- Botar o readonly
+*/
+
+// Evento do keyup no formulário com client name (onde tem o botão "add" ao lado para ele reduzir a opacidade)
+const inputClient = document.querySelector('[name="create_client"]')
+// Evento para adicionar um novo cliente
+const addClient = document.querySelector('.clients__add')
+const createForm = document.querySelector('.clients__create');
+// Cria primeiro os clients vazio se não tiver nada no local storage
+const clients = JSON.parse(localStorage.getItem('clients')) || [];
+let countId = 0;
+
+
+function create(name) {
+  // Temporário, o ID deve ser gerenciado pela base de dados/API
+  countId++
+  const client = document.createElement('div');
+  client.classList.add('client');
+  
+  const title = document.createElement('input');
+  title.classList.add('client__title');
+  title.setAttribute('type', 'text');
+  title.setAttribute('readonly', '');
+  title.setAttribute('value', name);
+  title.setAttribute('name', `client_${countId}`);
+  
+  const editButton = document.createElement('button');
+  editButton.classList.add('client__edit');
+  editButton.innerHTML = '<span class="material-icons">create</span>';
+  editButton.addEventListener('click', onEdit);
+  
+  const deleteButton = document.createElement('button');
+  deleteButton.classList.add('client__delete');
+  deleteButton.innerHTML = '<span class="material-icons">delete</span>';
+  deleteButton.addEventListener('click', onDelete);
+
+  client.append(title, editButton, deleteButton)
+
+  document.querySelector('.clients__container').append(client);
+
+}
+
+function onAdd(event) {
+  event.preventDefault();
+  create(inputClient.value)
+  clients.push(inputClient.value);
+  updateStorage();
+  inputClient.value = '';
+}
+
+function updateStorage() {
+  localStorage.setItem('clients', JSON.stringify(clients));
+}
+
+function onKeyUp() {
+  // Quando a string tá vazia = false, ou seja, não preciso usar inputClient.value.length
+  // Quando .length = 0, ele é falso
+  if(inputClient.value) {
+    addClient.removeAttribute('disabled');
+  } else {
+    addClient.setAttribute('disabled', '');
+  }
+}
+
+function onDelete() {
+  console.log('delete');
+}
+
+function onEdit() {
+  console.log('edit');
+}
+
+// Evento do keyup no formulário com client name (onde tem o botão "add" ao lado para ele reduzir a opacidade)
+inputClient.addEventListener('keyup', onKeyUp);
+
+// Evento para adicionar um novo cliente
+createForm.addEventListener('submit', onAdd);
+
+// For loop para criar os clients guardados
+for (const item of clients) {
+   create(item);
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // <div class="client">
+  //<input
+  //               type="text"
+  //               class="client__title"
+  //               value="Others"
+  //               name="client_1"
+  //               readonly
+  //             />
+  //<button class="client__edit">
+  //<span class="material-icons">create</span>
+  //</button>
+  //<button class="client__delete">
+  //<span class="material-icons">delete</span>
+  //</button>
+  //</div>
