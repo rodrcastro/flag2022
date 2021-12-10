5d5a0d14a6128c1308a90bdc4c2a465e1fc4cbfa
Rodrigo Castro <rodrcastro@gmail.com>
2021-12-10 21:35:26 +0000

    added billable rate function

--- tasks-webapp/js/components/team/team.js
+++ tasks-webapp/js/components/team/team.js
@@ -28,27 +28,65 @@ function createMember(member) {
     rate.innerHTML = "<span>€</span>";
 
     const input = document.createElement('input');
-    input.setAttribute('type', 'text');
+    input.setAttribute('type', 'number');
     input.setAttribute('name', `team_member_${member.id}`);
-    input.setAttribute('value', 0);
-
+    input.setAttribute('value', member.rate);
+    input.setAttribute('data-id', `${member.id}`);
+    input.addEventListener('blur', onInputBlur);
+    
     rate.prepend(input);
 
     const role = document.createElement('td');
-    role.innerHTML = '<span class="team__role">Owner</span>';
+    role.innerHTML = `<span class="team__role" data-role="${member.role}">${member.role}</span>`;
 
     row.append(user, email, rate, role);
 
     document.querySelector('.team__table tbody').append(row);
 }
 
+let membersData = JSON.parse(localStorage.getItem('members'));
+
+function onInputBlur(event) {
+    const element = event.target.closest('input');
+    // Outra forma de puxar essa info do target:
+    // const {target} = event; - Destructuring
+
+    const item = membersData.find(el => el.id == element.getAttribute(`data-id`));
+    item.rate = element.value;
+
+    localStorage.setItem('members', JSON.stringify(membersData));
+}
+
+function renderMembers(data) {
+    for (let item of data) {
+        createMember(item);
+    }
+}
+
 function getMembersFromAPI() {
+    const roles = ['Project Manager', 'Frontend Developer', 'Backend Developer', 'UI Designer', 'UX Designer', 'Tech Lead', 'QA'];
+
     fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
         .then(data => {
-            console.log(data);
-            // create member
+            for (let item of data) {
+                const random = Math.floor(Math.random() * roles.length);
+                item.role = roles[random];
+                createMember(item);
+            }
+            membersData = data;
+            // Adicionar ao Local storage e stringify da data
+            localStorage.setItem('members', JSON.stringify(data));
         });
 }
 
-getMembersFromAPI();
+if (localStorage.getItem('members')) {
+    renderMembers(JSON.parse(localStorage.getItem('members')));
+} else {
+    getMembersFromAPI();
+}
+
+// Billable rate
+// Campo de input
+// Colocamos lá um valor, ao sair desse campo, ele guarde o valor no user correto e atualize no storage
+
