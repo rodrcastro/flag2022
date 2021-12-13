4140e0f6656fdfa2ffafdfa138fc57fecbf0f6ef
Rodrigo Castro <rodrcastro@gmail.com>
2021-12-13 19:26:16 +0000

    created form html and css

--- tasks-webapp/js/main.js
+++ tasks-webapp/js/main.js
@@ -2,4 +2,5 @@ import './global.scss';
 import './components/header/header';
 import './components/sidebar/sidebar';
 //import './components/clients/clients';
-import './components/team/team';
+//import './components/team/team';
+import './components/contact-form/contact-form';
