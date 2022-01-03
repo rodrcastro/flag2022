fbbdd1a6cdf48413b23aebf3080d32256dee4cca
Rodrigo Castro <rodrcastro@gmail.com>
2022-01-03 21:38:08 +0000

    imported new components

--- tasks-angular/src/app/app.module.ts
+++ tasks-angular/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ClientsComponent } from './clients/clients.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContactComponent } from './contact/contact.component';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ClientsComponent,
-    SidebarComponent
+    SidebarComponent,
+    ContactComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,
