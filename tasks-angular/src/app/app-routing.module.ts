6092ea8229376f96be703fd02e1bce17345d3c27
Rodrigo Castro <rodrcastro@gmail.com>
2022-01-03 21:38:19 +0000

    added routes

--- tasks-angular/src/app/app-routing.module.ts
+++ tasks-angular/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientsComponent } from './clients/clients.component';
+import { ContactComponent } from './contact/contact.component';
+import { TeamComponent } from './team/team.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'clients', component: ClientsComponent},
+  {path: 'team', component: TeamComponent},
+  {path: 'contact-us', component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
