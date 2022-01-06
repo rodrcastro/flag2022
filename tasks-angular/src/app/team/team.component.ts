55f9798cf7657975cc1653f31c50400ecba6be6b
Rodrigo Castro <rodrcastro@gmail.com>
2022-01-06 04:51:45 +0000

    created adding users feature

--- tasks-angular/src/app/team/team.component.ts
+++ tasks-angular/src/app/team/team.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Member } from 'src/interfaces/members';
 import { MEMBERS } from 'src/mocks/members-mock';
+import { ToastService } from '../toast.service';
 
 @Component({
   selector: 'app-team',
@@ -9,9 +10,12 @@ import { MEMBERS } from 'src/mocks/members-mock';
 })
 export class TeamComponent implements OnInit {
   members = MEMBERS;
+  newMemberName = '';
+  newMemberEmail = '';
+  newMemberRole = '';
   rate = 0;
 
-  constructor() { }
+  constructor(private toastService: ToastService) { }
 
   ngOnInit(): void {
     const data = localStorage.getItem('members');
@@ -41,6 +45,18 @@ export class TeamComponent implements OnInit {
 
   onInputBlur(member: Member): void {
     this.rate = member.rate;
+    localStorage.setItem('members', JSON.stringify(this.members));
+  }
+
+  onAdd(): void {
+    this.members.push({id: new Date().getTime(), name: this.newMemberName, email: this.newMemberEmail, rate: 0, role: this.newMemberRole})
+
+    this.toastService.add(`Usuário ${this.newMemberName} criado com sucesso`, 'success');
+
+    this.newMemberName = '';
+    this.newMemberEmail = '';
+    this.newMemberRole = '';
+
     localStorage.setItem('members', JSON.stringify(this.members));
   }
 
