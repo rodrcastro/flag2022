a7c5bf60e4d16a995cdfac75d65f4ddea9a53c91
Rodrigo Castro <rodrcastro@gmail.com>
2022-01-14 21:55:01 +0000

    created basic functionality

--- tasks-react/src/components/clients/clients.js
+++ tasks-react/src/components/clients/clients.js
@@ -1,35 +1,73 @@
+import Client from '../client/client'
+
 import './clients.scss'
 
 function Clients() {
+  const data = [
+    {
+      id: 1,
+      name: 'Worten',
+      isActive: true
+    },
+    {
+      id: 2,
+      name: 'Fnac',
+      isActive: true
+    },
+    {
+      id: 3,
+      name: 'BMW',
+      isActive: false
+    },
+    {
+      id: 4,
+      name: 'Apple',
+      isActive: true
+    },
+    {
+      id: 5,
+      name: 'Microsoft',
+      isActive: true
+    },
+  ];
+  
   return (
-    <section class="clients">
-      <header class="clients__header">
-        <h2 class="clients__headline">Clients</h2>
-        <div class="clients__filters">
-          <select name="clients_filter" id="" class="clients__select">
+    <section className="clients">
+      <header className="clients__header">
+        <h2 className="clients__headline">Clients</h2>
+        <div className="clients__filters">
+          <select name="clients_filter" id="" className="clients__select">
             <option value="active">Show active</option>
             <option value="archived">Show archived</option>
             <option value="">Show all</option>
           </select>
           <input
             type="search"
-            class="clients__input"
+            className="clients__input"
             name="clients_search"
             placeholder="Search by name"
           />
         </div>
-        <form action="#nowhere" class="clients__create">
+        <form action="#nowhere" className="clients__create">
           <input
             type="text"
             name="create_client"
-            class="clients__input"
+            className="clients__input"
             placeholder="Add new client"
           />
-          <button type="submit" class="button clients__add" disabled>
+          <button type="submit" className="button clients__add" disabled>
             Add
           </button>
         </form>
       </header>
+      <div className="clients__container" data-status="active">
+      <p className="clients__label">Name</p>
+    
+        {data.map(client => (
+          <Client key={client.id} data={client}/>
+        ))}
+
+      </div>
     </section>
   )
 }
--- tasks-react/src/components/toast/toast.js
+++ tasks-react/src/components/toast/toast.js
@@ -1,15 +1,58 @@
-//import React from 'react'
-import './toast.scss'
+import { useState } from 'react';
+import './toast.scss';
 
 function Toast() {
+  const [count, setCount] = useState(5);
+  const [data, setData] = useState([
+    {
+      id: 1,
+      message: 'Toast info',
+      type: 'info'
+    },
+    {
+      id: 2,
+      message: 'Toast warning',
+      type: 'warning'
+    },
+    {
+      id: 3,
+      message: 'Toast error',
+      type: 'error'
+    },
+    {
+      id: 4,
+      message: 'Toast success',
+      type: 'success'
+    },
+  ])
+  // const updatedData = [...data, newData]
+
+  const onClose = (event) => {
+    console.log(event, 'click na toast');
+  }
+
+  const addToast = () => {
+    setData([...data,
+    {
+      id: count,
+      message: `Nova Toast com id ${count}`,
+      type: 'success'
+    }]);
+
+    setCount(count + 1);
+  }
+
   return (
-    <div className="toast">
-      <div className="toast__item">
-        <p className="toast__message">Lorem ipsum dolor sit amet.</p>
-        <button className="toast__close">
-          <span className="material-icons">close</span>
-        </button>
-      </div>
+    <div className='toast'>
+      <button onClick={addToast}>Add toast {count}</button>
+        {data.map(element => (
+          <div key={element.id} className="toast__item" data-status={element.type}>
+            <p className="toast__message">{element.message}</p>
+            <button className="toast__close" onClick={onClose}>
+              <span className="material-icons">close</span>
+            </button>
+          </div>
+        ))}
     </div>
   )
 }
