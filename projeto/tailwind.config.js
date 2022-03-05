a1f10ceb5d497df6ae3e0a0c63646ee2e7e85954
Rodrigo Castro <rodrcastro@gmail.com>
2022-03-05 20:40:51 +0000

    added theme to daisyui

--- projeto/tailwind.config.js
+++ projeto/tailwind.config.js
@@ -7,4 +7,21 @@ module.exports = {
     extend: {},
   },
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#c3a6ff',
+          secondary: '#8695b7',
+          accent: '#1FB2A6',
+          neutral: '#1d2433',
+          'base-100': '#2f3b54',
+          info: '#5ccfe6',
+          success: '#bae67e',
+          warning: '#ffcc66',
+          error: '#ef6b73',
+        },
+      },
+    ],
+  },
 };
